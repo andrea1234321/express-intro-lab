dc4f5a03a03be661c198ab7101a7852857b60905
Andrea <andrearewards@hotmail.com>
2023-04-11 14:18:18 -0400

    complete lab

---
+++ data/netflixshows-data.js
@@ -0,0 +1,9 @@
+const netflixShows= [
+  {show: 'Dead to me', stars: 4.5},
+  {show: 'You', stars: 4},
+  {show: 'Glory', stars: 3.5}
+]
+
+export{
+  netflixShows
+}
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express"
+import { netflixShows } from "./data/netflixshows-data.js"
 
 const app= express()
 
@@ -6,11 +7,21 @@ app.listen(3000, function(){
   console.log("listening on port 3000")
 })
 
-app.get('/', function(req,res){
-  res.send('<h1>hello, friend</h1>')
-})
+// app.get('/', function(req,res){
+//   res.send('<h1>hello, friend</h1>')
+// })
 
 app.set('view engine', 'ejs')
 app.get('/home', function(req, res) {
   res.render('home')
+})
+
+app.get('/netflixShows', function(req, res){
+  res.render('netflixShows/index', {
+    netflixShows: netflixShows
+  })
+})
+
+app.get('/', function(req,res){
+  res.redirect('/netflixShows')
 })
